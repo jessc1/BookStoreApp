3b708169e3b85fd75ac4e66130cdc87bf9445fea
jessc1 <mmariajessica@gmail.com>
2020-10-16 00:02:13 -0300

    add Orders with Stripe checkout

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'users.apps.UsersConfig',
     'pages.apps.PagesConfig',
     'books.apps.BooksConfig',
+    'orders.apps.OrdersConfig',
         # Third-party
     'crispy_forms',
     'allauth',
@@ -179,3 +180,7 @@ LOGIN_REDIRECT_URL='home'
 LOGOUT_REDIRECT_URL='home'
 
 DEFAULT_FROM_EMAIL = 'admin@djangobookstore.com'
+
+
+STRIPE_TEST_PUBLISHABLE_KEY=os.environ.get('STRIPE_TEST_PUBLISHABLE_KEY')
+STRIPE_TEST_SECRET_KEY = os.environ.get('STRIPE_TEST_SECRET_KEY')
